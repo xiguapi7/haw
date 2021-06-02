35e2740e8cd99d1906ef5f0310f6254b1a9e56a8
adan <adanz1015@outlook.com>
2021-06-02 13:23:24 +0800

    feat: 初始化Spring Boot工程

---
+++ haw-boot-server/src/main/java/cloud/xiguapi/haw/HawApplication.java
@@ -0,0 +1,12 @@
+package cloud.xiguapi.haw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawApplication.class, args);
+    }
+}
